0dca0459a10f3a020f96f25ceb031543e850139c
Gamadv <lealdev.py@gmail.com>
2024-06-07 20:41:14 -0300

    ✨ feat(App): Insert Router Provider - Toaster by Sonner - Helmet Provider and Helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import './globals.css'
+
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
 import { router } from './routes'
 
 export function App() {
-  return <RouterProvider router={router} />
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <Toaster richColors />
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  )
 }
