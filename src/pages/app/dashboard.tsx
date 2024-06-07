6542fd3a4ad36e737b3240e7af7a88d265766fb0
Gamadv <lealdev.py@gmail.com>
2024-06-07 20:42:48 -0300

    ✨ feat(Dash): Insert Helmet element

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -1,3 +1,10 @@
+import { Helmet } from 'react-helmet-async'
+
 export function Dashboard() {
-  return <div>Dashboard</div>
+  return (
+    <>
+      <Helmet title="Dashboard" />
+      <h1>Dashboard</h1>
+    </>
+  )
 }
