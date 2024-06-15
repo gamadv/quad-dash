197cd3c79b6f5b4aa0aa4769a1e013a06f6f87fc
Gamadv <lealdev.py@gmail.com>
2024-06-15 10:58:51 -0300

    ⚡️ feat(Routes): Insert new Routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,9 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
+import { NotFound } from './pages/404'
 import { Dashboard } from './pages/app/dashboard'
+import { History } from './pages/app/history'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
 
@@ -10,10 +12,15 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       {
         path: '/',
         element: <Dashboard />,
+      },
+      {
+        path: '/history',
+        element: <History />,
       },
     ],
   },
