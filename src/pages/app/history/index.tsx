800a189f4d862f2c701b1ceba17bca85306f339a
Gamadv <lealdev.py@gmail.com>
2024-06-15 10:49:06 -0300

    ⚡️ feat(HistoryPage): Create History Page - Create Table filter - Create Custom Table Row - Create row Details

---
+++ src/pages/app/history/index.tsx
@@ -0,0 +1,52 @@
+import { Helmet } from 'react-helmet-async'
+
+import { Pagination } from '@/components/pagination'
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+
+import { HistoryTableFilters } from './history-table-filter'
+import { HistoryTableRow } from './history-table-row'
+
+export function History() {
+  return (
+    <>
+      <Helmet title="Histórico" />
+
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Histórico</h1>
+
+        <div className="space-y-2.5">
+          <HistoryTableFilters />
+
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[64px]"></TableHead>
+                  <TableHead className="w-[140px]">Identificador</TableHead>
+                  <TableHead className="w-[180px]">Realizado há</TableHead>
+                  <TableHead className="w-[140px]">Status</TableHead>
+                  <TableHead>Cliente</TableHead>
+                  <TableHead className="w-[140px]">Total do pedido</TableHead>
+                  <TableHead className="w-[164px]"></TableHead>
+                  <TableHead className="w-[132px]"></TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {Array.from({ length: 10 }).map((_, i) => (
+                  <HistoryTableRow key={i} />
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+          <Pagination pageIndex={0} totalCount={105} perPage={10} />
+        </div>
+      </div>
+    </>
+  )
+}
