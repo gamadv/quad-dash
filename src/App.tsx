c02901a9958e79796a2e6bf5182ee41b2e08f2c8
Gamadv <lealdev.py@gmail.com>
2024-07-29 19:24:48 -0300

    ✨ feat(Providers): Update with ReactQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './styles/globals.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
 import { ThemeProvider } from '@/hooks/themeProvider'
 
+import { queryClient } from './lib/react-query'
 import { router } from './routes'
 
 export function App() {
@@ -14,7 +16,9 @@ export function App() {
       <ThemeProvider defaultTheme="dark" storageKey="quaddash-theme">
         <Helmet titleTemplate="%s | QuadDash" />
         <Toaster richColors />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   )
