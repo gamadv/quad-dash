1458b15661d8a1f37c2d2121b57ec240df198f48
Gamadv <lealdev.py@gmail.com>
2024-08-10 21:18:06 -0300

    🧪 feat(Setup): Create configs and add deps to start Tests - Vitest as main unit test - Testing Library for tests powerful and DOM manipulation - Happy Dom instead JSDOM (faster than)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'node:path'
 
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, InlineConfig, UserConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,4 +11,11 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    environment: 'happy-dom',
+  },
+} as UserConfig & {
+  test: InlineConfig
 })
