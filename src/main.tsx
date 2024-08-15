3e4b4c47541b442d5ad6ce89a0c8698d1261aedb
Gamadv <lealdev.py@gmail.com>
2024-08-15 19:58:16 -0300

    🧪 test(Unit): Pagination - Add MSW to mock services - Add @testing-library/user-event to user events

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { enableMSW } from './__tests__/mocks'
 import { App } from './App'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+enableMSW().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+})
